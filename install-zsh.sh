56dc94d75c0567bcc4b12ed0b952ce0eaa098327
savent <savent_gate@outlook.com>
2020-12-29 11:23:06 +0800

    add script install zsh

---
+++ install-zsh.sh
@@ -0,0 +1,26 @@
+#/bin/sh
+
+# install zsh and oh-my-zsh
+sudo apt install zsh fonts-powerline
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install powerlevel10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# set ZSH_THEME="powerlevel10k/powerlevel10k" in ~/.zshrc.
+sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
+
+# keep bashrc
+echo "source ~/.bashrc" >> ~/.zshrc
+
+# install plugins
+# autojump
+sudo apt install autojump
+echo '. /usr/share/autojump/autojump.sh'>>~/.zshrc
+
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# zsh-autosuggestions
+git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+sed -i 's/^plugins=.*/plugins=(git autojump zsh-syntax-highlighting zsh-autosuggestions)/g' ~/.zshrc
